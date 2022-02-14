e0f53cb66cd2df295042319e77da2cc81fd7925d
yoshiodei <yoshiodei@yahoo.com>
2022-02-14 06:56:22 +0000

    modify alert component

---
+++ src/components/Alert.jsx
@@ -0,0 +1,14 @@
+import './../styles/alert.css';
+
+const Alert = () => {
+    return (
+        <div className='alert'>
+            <div className='alert__div'>
+                <i class="bi bi-info-circle alert__icon"></i>
+                <h5 className='alert__header'>Tada! Get started with a free template. Can't find what you are looking for? Search from the 1000+ available templates</h5>
+            </div>
+        </div>
+    );
+}
+
+export default Alert;
